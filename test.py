c13394fa589ad0ff11e0a12f170f6ba927f6659b
Saurja <saurjaghoshofficial@gmail.com>
2021-05-12 23:02:32 +0530

    UI

--- test.py
+++
@@ -1,9 +0,0 @@
-import sqlite3 as sql
-
-con = sql.connect('patientData.db')
-cursor = con.cursor()
-# cursor.execute("SELECT * from PatientRecords.COLUMNS")
-cursor.execute("SELECT * FROM PatientRecords")
-col_name_list = [tuple[0] for tuple in cursor.description]
-
-print(col_name_list)
